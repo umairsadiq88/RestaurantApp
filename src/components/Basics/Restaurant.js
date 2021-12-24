66022b394284685f2c735f705bee7f8d493cdac6
umairsadiq88 <umairsadiq2010@gmail.com>
2021-12-24 17:33:28 +0500

    images

--- src/components/Basics/Restaurant.js
+++ src/components/Basics/Restaurant.js
@@ -8,8 +8,13 @@ const Restaurant = () => {
                 <div className='card'>
                     <div className='card-body'>
                         <span className='card-number card-circle subtle'>1</span>
-                   <span className='card-author subtle'>Breakfast</span>
+                        <span className='card-author subtle'>Breakfast</span>
+                        <h2 className='card-title'>Maggie</h2>
+                        <span className='card-description subtle'> Lorem   </span>
+                        <div className='card-read'>Read</div>
                     </div>
+                    {/* <img src={image} alt='images' className='ccard-media' /> */}
+                    <span className='card-tag subtle'> Order Now</span>
                 </div>
             </div>
         </>
